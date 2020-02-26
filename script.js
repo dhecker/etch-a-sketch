1cd07ee52a3d436701e54c5443f612c1d6b73fd9
Dave Hecker <dhecker@gmail.com>
2020-02-26 09:24:33 -0800

    jslint cleanup

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-console.log('Script Loaded'); 
 let gridSize = 16;
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
 drawGrid(gridSize);
-
 
 function drawGrid(gridSize)  {
 
-    for (row=0; row<gridSize ; row++) { // for each row
+    for (row=0; row<gridSize ; row++) { 
 
-        for (column=0; column<gridSize ; column++) { // for each column
+        for (column=0; column<gridSize ; column++) { 
 
-            var newdiv = document.createElement('div');   //create the individual div
-       //     newdiv.textContent  = row + "," + column;
+            let newdiv = document.createElement("div");   //create the individual div
             newdiv.id  = "row_" + row + "column+" + column;
-
             newdiv.style.backgroundColor = "red";
             newdiv.style.border = "1px solid #999999";
             newdiv.addEventListener("mouseover", hoverEffect);
-
             grid.appendChild(newdiv);
                             }
     }
-
-    // now set the grid dimensions in the wrapper
+            // now set the grid dimensions in the wrapper
             boxSize = (600 / gridSize);
             grid.style.gridTemplateColumns = ( "repeat(" + gridSize + "," + boxSize +"px)");
             grid.style.gridTemplateRows = ( "repeat(" + gridSize + "," + boxSize +"px)");
@@ -30,21 +24,20 @@ function drawGrid(gridSize)  {
 }
 
 function resizeGrid() {
-    gridSize = prompt ("How large should the grid be?")
-    drawGrid(gridSize)
+    let gridSize = prompt ("How large should the grid be?");
+    drawGrid(gridSize);
 }
 
 function resetGrid() {
     
-    grid.querySelectorAll('*').forEach(n => n.remove());
+    grid.querySelectorAll("*").forEach(n => n.remove());
     drawGrid(gridSize);
     
 }
 
 function  hoverEffect (e){   
 
-    hovered = document.getElementById(this.id);
-    console.log(hovered)
+    let hovered = document.getElementById(this.id);
     hovered.style.backgroundColor= "#000000";
 }
 
